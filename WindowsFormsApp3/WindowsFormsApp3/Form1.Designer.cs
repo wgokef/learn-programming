09dd9700aaaff75e3cc52ecf442a7e80d91848f9
wgokef <verlevskaasofia@gmail.com>
2025-03-13 14:05:21 +0300

    написала проект массивы

---
+++ WindowsFormsApp3/WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(53, 97);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(143, 181);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "-1,5\r\n0,1\r\n12\r\n0\r\n-2,2\r\n0,5\r\n-1\r\n0,3";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(270, 136);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(78, 91);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(469, 118);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 160);
+            this.listBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ WindowsFormsApp3/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // определение количества строк в textbox (TxBx)
+           int n = textBox1.Lines.Count();
+            // объявление нового одномерного массива a []
+            double [] a = new double[n];
+            // заполнение массива а данными из textbox
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = double.Parse(textBox1.Lines[i]);
+            }
+
+
+           
+
+            double t = a[0];
+            double b = a[0];
+
+            for (int i = 1; i < a.Length; i++)
+            {
+                if (t > a[i])
+                {
+                    t = a[i];
+                }
+            }
+
+
+            for (int i = 1; i < a.Length; i++)
+            {
+                if (b < a[i])
+                {
+                    b = a[i];
+                }
+            }
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] < 0)
+                {
+                    a[i] = t;
+                }
+                else if (a[i] > 0)
+                {
+                    a[i] = b;
+                }
+            }
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                listBox1.Items.Add(a[i]);
+            }
+        }
+    }
+}
